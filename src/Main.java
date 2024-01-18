a5c6d1903ae89dbd8b9e9476d12a91fd570dbf95
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-01-19 00:00:36 +0300

    Сделано смещение массива

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 import Task1.CaesarСoder;
+import Task2.ArrayOffseter;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
         String decodedString = new CaesarСoder().code("Привет, мир!", 5, true);
         System.out.println(decodedString);
         String encodedString = new CaesarСoder().code(decodedString, 5, false);
         System.out.println(encodedString);
+
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(
+                new ArrayOffseter().offset(arr, 5)
+        ));
+
+
+
     }
 }
---
+++ src/Task2/ArrayOffseter.java
@@ -0,0 +1,18 @@
+package Task2;
+
+import java.util.Arrays;
+
+public class ArrayOffseter {
+
+    public Integer[] offset(Integer[] arr, Integer offset) {
+        Integer[] tail = Arrays.copyOfRange(arr,arr.length - offset,arr.length);
+        Integer[] newArr = Arrays.copyOf(tail,arr.length);
+        for (int i = 0; i < arr.length - offset; i++) {
+            newArr[i + offset] = arr[i];
+        }
+        return newArr;
+    }
+
+
+
+}
