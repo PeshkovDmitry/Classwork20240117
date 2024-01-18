9110eefe08b90d5f458f3e1e78a98b412bb566c9
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-01-18 23:35:37 +0300

    Сделан шифр Цезаря

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import Task1.CaesarСoder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String decodedString = new CaesarСoder().code("Привет, мир!", 5, true);
+        System.out.println(decodedString);
+        String encodedString = new CaesarСoder().code(decodedString, 5, false);
+        System.out.println(encodedString);
     }
 }
---
+++ src/Task1/CaesarСoder.java
@@ -0,0 +1,20 @@
+package Task1;
+
+/**
+ * Класс, реализующий шифратор по "шифру Цезаря"
+ */
+
+public class CaesarСoder {
+
+    public String code(String text, Integer offset, boolean decode) {
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (decode) {
+                chars[i] -= offset;
+            } else {
+                chars[i] += offset;
+            }
+        }
+        return new StringBuilder().append(chars).toString();
+    }
+}
